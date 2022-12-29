4660c8ebc40c10a33417755188599f237a0d2be5
Aman <mr7kitkat@gmail.com>
2022-12-29 13:29:41 +0530

    make change in html

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,31 @@
 import React from "react";
-import Form from "./Form";
-import Canvas from "./Canvas";
+import data from "../data/sampledata";
 
 export default function () {
+  const [image, set_image] = React.useState("");
+
+  function getMeme() {
+    const memes = data.data.memes;
+    const maxLen = memes.length;
+    const randomIdx = Math.floor(Math.random() * maxLen);
+    set_image(memes[randomIdx].url);
+  }
+
   return (
     <main className="body">
-      <Form />
-      <Canvas />
+      <div className="ui-form">
+        <div className="inputArea">
+          <input type="text" className="u-input" />
+          <input type="text" className="d-input" />
+        </div>
+
+        <button onClick={getMeme} type="submit" className="submit">
+          Get a new meme image
+        </button>
+      </div>
+      <section className="canvas">
+        <img src={image} alt="" className="meme" />
+      </section>
     </main>
   );
 }
